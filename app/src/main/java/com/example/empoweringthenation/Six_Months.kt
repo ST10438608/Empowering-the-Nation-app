182c01feeebc836e626c08b000264f141f45e19c
ST10438608 <160500321+ST10438608@users.noreply.github.com>
2024-09-09 13:47:29 +0200

    organised code

--- app/src/main/java/com/example/empoweringthenation/Six_Months.kt
+++ app/src/main/java/com/example/empoweringthenation/Six_Months.kt
@@ -11,18 +11,13 @@ import android.widget.Spinner
 import android.widget.TextView
 import android.widget.Toast
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Six_Months.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class Six_Months : Fragment() {
-    // TODO: Rename and change types of parameters
+
     private var param1: String? = null
     private var param2: String? = null
 
@@ -69,14 +64,7 @@ class Six_Months : Fragment() {
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Settings.
-         */
+
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             Six_Months().apply {
--- app/src/main/java/com/example/empoweringthenation/Six_Weeks.kt
+++ app/src/main/java/com/example/empoweringthenation/Six_Weeks.kt
@@ -11,18 +11,13 @@ import android.widget.Spinner
 import android.widget.TextView
 import android.widget.Toast
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Six_Weeks.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class Six_Weeks : Fragment() {
-    // TODO: Rename and change types of parameters
+
     private var param1: String? = null
     private var param2: String? = null
 
@@ -68,14 +63,7 @@ class Six_Weeks : Fragment() {
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Settings.
-         */
+
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             Six_Weeks().apply {
