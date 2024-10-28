0d331ffd5541269b13d53bb3c3f8421e60baba70
ST10438608 <160500321+ST10438608@users.noreply.github.com>
2024-10-28 20:38:14 +0200

    adjusted content and design of courses

--- app/src/main/java/com/example/empoweringthenation/Sewing.kt
+++ app/src/main/java/com/example/empoweringthenation/Sewing.kt
@@ -3,12 +3,14 @@ package com.example.empoweringthenation
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class Sewing : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
